5b2a4b15441aa5f33f8616afec9685940d92239a
conleye44 <167717594+conleye44@users.noreply.github.com>
2025-04-02 18:11:26 -0600

    init

---
+++ import shutil.py
@@ -0,0 +1,33 @@
+import shutil
+import os
+from Classifier.py import classify_image
+
+Main_dir="C:\\Users\\sys4dmin\\Documents\\Development\\NotHotDog\\Testing"
+HD_dir="C:\\Users\\sys4dmin\\Documents\\Development\\NotHotDog\\Testing\\Hotdog"
+Other_dir="C:\\Users\\sys4dmin\\Documents\\Development\\NotHotDog\\Testing\\Other"
+
+def get_images_from_directory(directory, valid_extensions=(".jpg", ".jpeg", ".png")):
+    #Returns a list of image file paths from the specified directory.
+    return [os.path.join(directory, f) for f in os.listdir(directory) 
+            if f.lower().endswith(valid_extensions)]
+
+
+def move_image(image_path, destination_folder):
+    #Moves an image to the specified folder, creating the folder if needed.
+    os.makedirs(destination_folder, exist_ok=True)  # Ensure the folder exists
+    shutil.move(image_path, os.path.join(destination_folder, os.path.basename(image_path)))
+
+
+images = get_images_from_directory(Main_dir)
+
+for image in images:
+        is_hotdog = classify_image(image)
+        is_hotdog = False  
+
+        # Move the image to the appropriate folder
+        if is_hotdog:
+            move_image(image, HD_dir)
+        else:
+            move_image(image, Other_dir)
+
+
